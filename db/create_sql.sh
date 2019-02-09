109a66d89b0c314b00fc59fe1a2cdad3348affbe
tanden <sumita.takaki@gmail.com>
2019-02-09 17:21:02 +0900

    [new] create .sql file with timestamp prefix

---
+++ db/create_sql.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if [ $# -eq 0 ] ; then 
+    echo 'usage: ./create_sql.sh <sql_file_name>'
+    exit 1
+fi
+
+timestamp=`date +%Y%m%d%H%M`
+up=./sql/${timestamp}_${1}.up.sql
+down=./sql/${timestamp}_${1}.down.sql
+
+touch ${up}
+touch ${down}
+
+# 念の為ファイルがあるかどうかチェック
+if [ -f ${up} ] && [ -f ${down} ] ; then
+    echo 'create sql file: success!'
+    exit 0
+else
+    echo 'create sql file: fail...'
+    exit 1
+fi
+
+# 空の.sqlのファイルを作成する
+# example 
+# $ ./create_sql.sh create_table
+# $ ls 
+# 201902091202_create_table.up.sql
+# 201902091202_create_table.down.sql
