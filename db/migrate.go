efc9bb62f73c438da1b6de53fc32b5a964b9a343
tanden <sumita.takaki@gmail.com>
2019-02-09 04:24:01 +0000

    [modify] migrate.go use command line args

--- db/migrate.go
+++ db/migrate.go
@@ -1,18 +1,46 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/mysql"	
+	_ "github.com/golang-migrate/migrate/v4/database/mysql"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
+var (
+	command = flag.String("exec", "no command", "set up or down as a command to exec sql")
+)
+
 func main() {
+
 	m, err := migrate.New(
 		"file://sql/",
 		"mysql://teixy:teixy@tcp(0.0.0.0:3306)/teixy_article")
+
 	if err != nil {
 		fmt.Println("err", err)
 	}
-	m.Steps(-1)
+
+	version, dirty, err := m.Version()
+	fmt.Println(version)
+	fmt.Println(dirty)
+	fmt.Println(err)
+
+	flag.Parse()
+	if *command == "up" {
+		fmt.Println("command: exec up")
+		err := m.Up()
+		if err != nil {
+			fmt.Println("err", err)
+		}
+	}
+
+	if *command == "down" {
+		fmt.Println("command: exec down")
+		err := m.Down()
+		if err != nil {
+			fmt.Println("err", err)
+		}
+	}
 }
