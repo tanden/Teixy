1a3eaefdc0a29e51a41cda02c1460fbf3cd087ff
tanden <sumita.takaki@gmail.com>
2019-02-11 19:51:42 +0900

    [modify] allow get and post from any origin temporarily

--- go/main.go
+++ go/main.go
@@ -12,6 +12,11 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.POST},
+	}))
+
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, "Hello World!")
 	})
