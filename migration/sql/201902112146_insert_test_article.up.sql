4328c2e59ce98ca9d78e0da72ef536a974911500
tanden <sumita.takaki@gmail.com>
2019-02-11 22:00:54 +0900

    [new] insert one article as test data

---
+++ migration/sql/201902112146_insert_test_article.up.sql
@@ -0,0 +1,15 @@
+INSERT INTO articles (
+    max_score,
+    min_score,
+    title,
+    punch_line,
+    content,
+    status
+) values (
+    800,
+    500,
+    'TOEIC L & R TEST 出る単特急 金のフレーズ (TOEIC TEST 特急シリーズ)',
+    '金色ロード！単語帳の王道',
+    'これはテスト記事です',
+    0
+)
