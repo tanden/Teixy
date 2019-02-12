295f6b68e89e92e4dd7a7ba01ef706373a706d99
tanden <sumita.takaki@gmail.com>
2019-02-12 21:01:27 +0900

    [modify] use const db connection info

--- go/db/db_connection.go
+++ go/db/db_connection.go
@@ -6,10 +6,13 @@ import (
 	"log"
 )
 
+const Driver = "mysql"
+const ConnInfoTeixyArticle = "teixy:teixy@tcp(db_article:3306)/teixy_article"
+
 //Create mysql connection
 //to teixy_article database
 func CreateConectionArticle() *sqlx.DB {
-	db, err := sqlx.Connect("mysql", "teixy:teixy@tcp(db_article:3306)/teixy_article")
+	db, err := sqlx.Connect(Driver, ConnInfoTeixyArticle)
 	if err != nil {
 		log.Fatal(err)
 	}
