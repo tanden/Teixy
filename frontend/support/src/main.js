522c5d6c0d6f9e0c35c150345d06904c90a06c6d
tanden <sumita.takaki@gmail.com>
2019-02-18 21:13:52 +0900

    [modify] add firebase api keys : these info is not confidencial

--- frontend/support/src/main.js
+++ frontend/support/src/main.js
@@ -2,8 +2,20 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store'
 import router from './router'
+import firebase from 'firebase/app'
 
 Vue.config.productionTip = false;
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyCz5_h_UjxU5s-pmEuTHOQ5HkaogTexOLQ",
+  authDomain: "teixy-support.firebaseapp.com",
+  databaseURL: "https://teixy-support.firebaseio.com",
+  projectId: "teixy-support",
+  storageBucket: "teixy-support.appspot.com",
+  messagingSenderId: "891516084242"
+};
+firebase.initializeApp(config);
 
 new Vue({
   store,
