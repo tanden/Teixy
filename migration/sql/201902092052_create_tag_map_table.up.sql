2e36b9b48c1250a4dbcf52656d8a67b91334a4f0
tanden <sumita.takaki@gmail.com>
2019-02-24 16:39:23 +0900

    [modify] set foreign key name

--- migration/sql/201902092052_create_tag_map_table.up.sql
+++ migration/sql/201902092052_create_tag_map_table.up.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS `tag_map` (
   `ctime`      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`book_id`,`tag_id`),
   KEY (`tag_id`),
-  FOREIGN KEY (`book_id`) REFERENCES books (`book_id`),
-  FOREIGN KEY (`tag_id`) REFERENCES tags (`tag_id`)
+  CONSTRAINT `fk_tag_map_books_book_id` FOREIGN KEY (`book_id`) REFERENCES books (`book_id`),
+  CONSTRAINT `fk_tag_map_tags_tag_id`   FOREIGN KEY  (`tag_id`) REFERENCES tags (`tag_id`)
 ) ENGINE=INNODB;
--- migration/sql/201902092115_create_author_map_table.up.sql
+++ migration/sql/201902092115_create_author_map_table.up.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS `author_map` (
   `ctime`      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`book_id`, `author_id`),
   KEY `author_id` (`author_id`),
-  FOREIGN KEY (`book_id`) REFERENCES books (`book_id`),
-  FOREIGN KEY (`author_id`) REFERENCES authors (`author_id`)
+  CONSTRAINT `fk_author_map_books_book_is`    FOREIGN KEY (`book_id`)   REFERENCES books (`book_id`),
+  CONSTRAINT `fk_author_map_athors_author_id` FOREIGN KEY (`author_id`) REFERENCES authors (`author_id`)
 ) ENGINE=INNODB;
